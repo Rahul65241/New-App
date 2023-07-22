caf48ddd35e1827e0115cce25424f054f1cfd0bd
Rahul <rahulkr11886@gmail.com>
2023-07-23 01:51:57 +0530

    New Branch Rahul created

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: 'yellow', fontWeight: 'bold', fontSize: 25 }}>Let's Start</Text>
+      <Text style={{ color: 'yellow', fontWeight: 'bold', fontSize: 25 }}>Let's Start Rahul</Text>
       <StatusBar style="auto" />
     </View>
   );
